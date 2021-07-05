d7880184318cf61e5772dc56615330ec105538e7
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-04 20:07:55 -0700

    Brew formula update for sqsmover version v0.4.0

--- Formula/sqsmover.rb
+++ Formula/sqsmover.rb
@@ -2,24 +2,24 @@
 class Sqsmover < Formula
   desc "AWS SQS Message mover."
   homepage "https://github.com/mercury2269/sqsmover"
-  version "0.3.14"
+  version "0.4.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/mercury2269/sqsmover/releases/download/v0.3.14/sqsmover_Darwin_x86_64.tar.gz"
-    sha256 "44bf483f81bd59667215d9b8327a560491b82a43dc39d446d8aa9f48234bb97b"
+    url "https://github.com/mercury2269/sqsmover/releases/download/v0.4.0/sqsmover_Darwin_x86_64.tar.gz"
+    sha256 "84e61d30f28819830d81aa4075d8aa371f2ad32768e3ef52b4bb4f17f54ca5be"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/mercury2269/sqsmover/releases/download/v0.3.14/sqsmover_Linux_x86_64.tar.gz"
-      sha256 "f152e4d884c8a89c492d44e8a3266b164725fe05f261d9782e0104b30d9a2310"
+      url "https://github.com/mercury2269/sqsmover/releases/download/v0.4.0/sqsmover_Linux_x86_64.tar.gz"
+      sha256 "870bd37ed21618d4b429cb60ba10fa17ba6e48bf779df59eea0119b75155745c"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mercury2269/sqsmover/releases/download/v0.3.14/sqsmover_Linux_arm64.tar.gz"
-        sha256 "cc03580e381e8674e154d1cf0128eb039eab82f38faed80b9cc59913eba3c282"
+        url "https://github.com/mercury2269/sqsmover/releases/download/v0.4.0/sqsmover_Linux_arm64.tar.gz"
+        sha256 "e6c6a6e3a6c49469388ffd7241684c3f5bd4a086419952b0f379080e240393d3"
       else
-        url "https://github.com/mercury2269/sqsmover/releases/download/v0.3.14/sqsmover_Linux_armv6.tar.gz"
-        sha256 "17446be2d4af4b0a422cf527403909d66436f781c73c202b556d097a3ce0b71d"
+        url "https://github.com/mercury2269/sqsmover/releases/download/v0.4.0/sqsmover_Linux_armv6.tar.gz"
+        sha256 "a8aacb66a740d8e7f7b46bb736d5fcf898661a9dd073cab8cb84c2ac9d9b23f6"
       end
     end
   end
